7182561882b54e7a021f9830ecfc44e61f14b86b
RikyIsola <riky.isola@gmail.com>
2020-10-18 17:40:37 +0200

    add support for pypi

---
+++ scripts/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='whatsappwebbot',
+   version='0.0.1',
+   description='A python telegram bot to forward whatsapp messages to telegram',
+   license="GPL-3",
+   long_description=long_description,
+   author='Riccardo Isola',
+   author_email='riky.isola@gmail.com',
+   url="https://github.com/RikyIsola/WhatsappWebBot",
+   packages=find_packages(),
+   install_requires=['selenium', 'python-telegram-bot'],
+   scripts=['scripts/whatsappwebbot']
+)
